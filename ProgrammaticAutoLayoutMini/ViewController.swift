2878e9b771b785e144c1edbf0abbf0f4fffddbb4
Jordan Nelson <nelsonjordan20@yahoo.com>
2016-02-19 10:05:51 -0700

    FinishedProject

--- ProgrammaticAutoLayoutMini/ViewController.swift
+++ ProgrammaticAutoLayoutMini/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        createButton()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createButton() {
+    
+        button.setTitle("Apply Now", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor(patternImage: UIImage(named: "BlueDevButton")!)
+        button.layer.cornerRadius = 8
+        button.layer.borderColor = UIColor.blueColor().CGColor
+        button.layer.borderWidth = 2.0
+        self.view.addSubview(button)
+        addButtonContstraints()
     }
-
+    
+    func addButtonContstraints() {
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let horizontalConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+        
+        view.addConstraint(horizontalConstraint)
+        
+        let verticalConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+        
+        view.addConstraint(verticalConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Width, multiplier: 0.75, constant: 0)
+        view.addConstraint(widthConstraint)
+        
+        let heightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        
+        view.addConstraint(heightConstraint)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
